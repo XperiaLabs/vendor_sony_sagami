182241ac0baf6c1239f842284f03f6f3c813f76f
saku-bruh <notwolfyy@gmail.com>
2023-11-24 14:23:20 +0200

    sagami: common: Nuke WFD Leftovers
    * We use AOSP WFD, not Qualcomm's WFD
    
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -1931,12 +1931,10 @@ PRODUCT_PACKAGES += \
     QtiTelephonyService \
     embms \
     uceShimService \
-    WfdService \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
     tcmclient \
-    WfdCommon \
     audiosphere \
     com.android.hotwordenrollment.common.util \
     com.qti.dpmframework \
